387c15f4bad03e8804b2a48be734cadef46eda6a
Khaireddine Sarraj <sarraj.khaireddine@gmail.com>
2021-10-08 14:33:52 +0100

    last

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,11 +1,30 @@
 import { memo } from "react";
 import Stars from "../../Components/Stars/Stars";
 import '../../Styles/App/App.css';
-
+import {Redirect, Route, Switch, useHistory, useLocation, useRouteMatch} from "react-router-dom";
+const RestrictedRoute = ({component: Component, location, tokens, ...rest}) =>
+  <Route
+    {...rest}
+    render={props =>
+      tokens
+        ? <Component {...props} />
+        : <Redirect
+          to={{
+            pathname: '/signin',
+            state: {from: location}
+          }}
+        />}
+  />;
 const App = () => {
+    let match = useRouteMatch();
     return (
       <div className="App">
-        <Stars />
+        <Switch>
+          <Route exact path='/signin' component={SignIn}/>
+          <Route exact path='/signup' component={SignUp}/>
+          <RestrictedRoute path={`${match.url}`} tokens={tokens} location={location}
+                           component={MainApp}/> 
+        </Switch>
       </div>
     );
   }
